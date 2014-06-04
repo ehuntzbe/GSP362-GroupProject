63895809f1c41a59340b7328f598a3c9527e666c
BigGuy00 <darkcarravenger@yahoo.com>
2014-06-04 02:21:27 -0400

    Change 5
    This is the main guts of the loading of the NPC's. What happens is now
    Rooms also have a number that tells how many NPC's are there. If that
    number is greater than zero it loads the NPC's from a file using the
    room's name as the key to load the right enemies into the room.

--- Merged Project/World.cpp
+++ Merged Project/World.cpp
@@ -26,11 +26,101 @@ void World::DisplayRoom(int p_roomID)
 	if (DISPLAY_ROOM_ID)
 		cout << "ID: " << p_roomID << endl;
 	cout << m_world[p_roomID]->GetName() << endl;
-	cout << m_world[p_roomID]->GetDesc() << endl;
+	cout << m_world[p_roomID]->GetDesc() << endl;	
+	if(m_world[p_roomID]->GetNumNpc() > 0)
+	{
+		cout << m_world[p_roomID]->GetNumNpc() << endl;
+		for (int a = 0; a < m_world[p_roomID]->GetNumNpc(); a++)
+		{
+			vector<Character> temp;
+			Combatant tempStats;
+						
+			temp = m_world[p_roomID]->NpcList();
+			tempStats = temp[a].GetStats();
+
+			cout << "Name: "<< temp[a].GetName() <<endl;				
+			cout << "Max HP: "<< tempStats.GetMaxHp() <<endl;		
+			cout << "Max MP: "<< tempStats.GetMaxMp() <<endl;		
+			cout << "Max Str: "<< tempStats.GetMaxStr() <<endl;		
+			cout << "Max Agi: "<< tempStats.GetMaxAgi() <<endl;		
+			cout << "Max As: "<< tempStats.GetMaxAs() <<endl;		
+			cout << "HP: "<< tempStats.GetHp() <<endl;			
+			cout << "MP: "<< tempStats.GetMp() <<endl;			
+			cout << "Str: "<< tempStats.GetStr() <<endl;			
+			cout << "Agi: "<< tempStats.GetAgi() <<endl;			
+			cout << "As: "<< tempStats.GetAs() <<endl;
+			cout << "Exp: "<< tempStats.GetExp() <<endl <<endl;
+		}
+	}
 	cout << "EXITS:"; m_world[p_roomID]->DisplayExits(); cout << endl;
 }
 
-void World::LoadRoomsFromFile(string p_fileName)
+void World::LoadNpcsFromFile(string npc_fileName, Room* test)
+{
+	//test->NpcResize(test->GetNumNpc());
+	ifstream myfile (npc_fileName);
+	string m_line;
+	int tempcount = 0;
+
+	if (myfile.is_open())
+	{
+		//getline (myfile,m_line);
+		while ( getline (myfile,m_line) )
+		{
+			if(m_line == test->GetName())
+			{
+				for(int a = 0; a < test->GetNumNpc(); a++)
+				{
+				Character m_npc;
+				Combatant temp;
+
+				getline (myfile,m_line);
+				m_npc.SetName(m_line);
+				getline (myfile,m_line);
+				temp.SetMaxHp(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetMaxMp(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetMaxStr(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetMaxAgi(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetMaxAs(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetHp(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetMp(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetStr(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetAgi(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetAs(atoi(m_line.c_str()));
+				getline (myfile,m_line);
+				temp.SetExp(atoi(m_line.c_str()));
+
+				m_npc.SetStats(temp);
+
+				test->NpcAdd(m_npc);
+
+				tempcount++;
+				}
+			}
+
+			/*else
+			{
+				cout << "Unable to Load file";
+			}*/
+			if(tempcount == test->GetNumNpc())
+			{
+				myfile.close();
+			}
+		}
+	}
+	else cout << "Unable to open file Load failed"; 
+}
+
+void World::LoadRoomsFromFile(string p_fileName, string npc_fileName)
 {
 	int counter = -1;
 	string line;
@@ -56,6 +146,13 @@ void World::LoadRoomsFromFile(string p_fileName)
 				m_world[counter]->SetName(line);
 				getline (myfile, line);
 				m_world[counter]->SetDesc(line);
+				getline (myfile, line);
+				m_world[counter]->SetNumNpc(line);
+
+				if(m_world[counter]->GetNumNpc() > 0)
+				{
+					LoadNpcsFromFile(npc_fileName, m_world[counter]);
+				}
 			}
 		}
 		myfile.close();
@@ -86,4 +183,9 @@ void World::ConnectRooms()
 World::~World()
 {
 	delete m_world;
+}
+
+int World::RoomCount()
+{
+	return m_roomCount;
 }
