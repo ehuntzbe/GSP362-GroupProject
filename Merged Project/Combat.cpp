4bbce1fdc78b2096fd97afdf19b949bfd69ccb00
BigGuy00 <darkcarravenger@yahoo.com>
2014-06-04 20:21:38 -0400

    Revert "a glitch"
    This reverts commit a60292151f73952dc56ad78859c3a2c2b6ab8603.

---
+++ Merged Project/Combat.cpp
@@ -0,0 +1,114 @@
+// Eric H. 6/4
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include "Combat.h"
+using namespace std;
+
+enum combat_outcome {DEAD=0, FLED, KILL};
+
+
+int Combat(Combatant player, Combatant enemy)
+{	// Plays a combat.
+	// Returns true if player defeats enemy, and false otherwise
+
+	signed int damage = 0, hp = 0;
+	bool player_guard = false, enemy_guard = false;
+	bool valid = false;
+	string userInput;
+
+	while(1)
+	{
+		player_guard = false;
+		valid = false;
+
+		// ask for command input from player, and execute it
+		do {
+			getline(cin, userInput);
+			if(	(userInput == "a") || (userInput == "attack") ||
+				(userInput == "g") || (userInput == "guard") ||
+				(userInput == "f") || (userInput == "flee")
+				)
+				valid = true;
+		} while(valid == false);
+
+		// attack
+		if((userInput == "a") || (userInput == "attack")) {
+			// attack formula for player:
+			//		(R + s/2)*(m+1)
+
+			// R = random number (ranging from 1 to 4)
+			// s = strength
+			// m = magic
+
+			damage = ((rand() % 4 + 1) + player.GetStr()/2) * (1 + player.GetMp()/2);
+			hp = enemy.GetHp();
+			hp = hp - damage;
+			enemy.SetHp(hp);
+		}
+
+		// guard
+		if((userInput == "g") || (userInput == "guard")) {
+			player_guard = true;
+		}
+
+		// flee
+		if((userInput == "f") || (userInput == "flee")) {
+			if(rand() % 3 == 0)	return FLED;
+		}
+
+		// if enemy's hp is 0 or less, kill enemy and return true
+		if(enemy.GetHp() <= 0) {
+			return KILL;
+		}
+		
+		enemy_guard = false;
+
+		// let enemy perform random attack on player
+		int enemy_choice = rand() % 3;
+		switch(enemy_choice) {
+		case 0:
+			// attack
+
+			// attack formula for enemy:
+			//		(R + s/2)*(m+1)
+
+			// R = random number (ranging from 1 to 3)
+			// s = strength
+			// m = magic
+
+			damage = ((rand() % 3 + 1) + player.GetStr()/2) * (1 + player.GetMp()/2);
+
+			hp = player.GetHp();
+			hp = hp - damage;
+			player.SetHp(hp);
+			break;
+
+		case 1:
+			// guard
+			enemy_guard = true;
+			break;
+
+		case 2:
+			// flee
+			if(rand() % 4 == 0)	return FLED;
+			break;
+		}
+
+		// if player's hp is 0 or less, kill player and return false
+		if(player.GetHp() <= 0) {
+			return DEAD;
+		}
+	}
+}
+
+Combatant Combatant::operator+(Combatant p_other)
+{
+	Combatant combinedStats;
+	combinedStats.m_currentAgi = m_currentAgi + p_other.m_currentAgi;
+	combinedStats.m_currentAs = m_currentAs + p_other.m_currentAs;
+	combinedStats.m_currentStr = m_currentStr + p_other.m_currentStr;
+	combinedStats.m_currentHp = m_currentHp + p_other.m_currentHp;
+	combinedStats.m_currentMp = m_currentMp + p_other.m_currentMp;
+	return combinedStats;
+}
---
+++ Merged Project/Interface.cpp
@@ -0,0 +1,61 @@
+#include "Interface.h"
+
+Interface* Interface::m_interfaceInstance = NULL;
+
+Interface::Interface()
+{
+	WORLD->LoadRoomsFromFile("WorldTest.txt", "NPC.txt");
+}
+
+Interface::~Interface()
+{
+
+}
+
+Interface* Interface::GetInstance()
+{
+	if (m_interfaceInstance == NULL)
+	{
+		m_interfaceInstance = new Interface();
+	}
+	return m_interfaceInstance;
+}
+
+void Interface::MainGame()
+{
+	string input = "";
+	vector<string> parsedInput;
+	bool quitGame = false;
+	while(!quitGame)
+	{
+		cout << ">";
+		getline(cin, input);
+		for (int i = 0; i < input.size(); i++)
+			tolower(input[i]);
+		parsedInput = ParseInput(input);
+		command cmd = CommandControl::GetInstance()->GetCommand(parsedInput.front());
+		string target = parsedInput.back();
+		if (cmd == LOOK)
+			Look();
+		if (cmd == GO_NORTH)
+			North();
+		if (cmd == GO_SOUTH)
+			South();
+		if (cmd == GO_EAST)
+			East();
+		if (cmd == GO_WEST)
+			West();
+		if (cmd == GO_DOWN)
+			Down();
+		if (cmd == GO_UP)
+			Up();
+		if (cmd == KILL)
+		{
+			//Character* target = FindChar(parsedInput.back()); //Need a FindChar function that will check through the characters in the room and return the one that has the target the player gave
+			//if(target != NULL))
+			//	Kill(target);
+		}
+		if (cmd == SCORE)
+			Score();
+	}
+}
