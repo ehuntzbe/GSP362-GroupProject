2d315531eeed7f6312eb5a2e5e4e9fffe17de24a
EricH <skyfjet2@yahoo.com>
2014-06-07 00:06:32 -0400

    Patched the Ability command

--- Merged Project/CommandFunctions.cpp
+++ Merged Project/CommandFunctions.cpp
@@ -144,118 +144,40 @@ void Abilities()
 {
 	vector<ability> pcAbil = PC->GetAbilities();
 
-	bool bContinue = true;
 	int userInput; // Holds the key pressed by the user
-
-	int i = 0;
-	for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
+	while (true)
 	{
-		i++;
-		if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
+		int i = -1;
+		for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
 		{
-			cout << "Ability " << i << ": " << it->m_name << endl;
-			cout << endl;
+			i++;
+			if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
+			{
+				cout << "Ability " << i+1 << ": " << it->m_name << endl;
+			}
 		}
-		cout << "Enter the number of the ability to see its full effects: ";
+		cout << "Enter the number of the ability to see its full effects or 0 to quit: ";
 		cin >> userInput;
 		cin.clear();
-
-		if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
+		userInput--;
+		if (userInput == -1)
+			break;
+		if(PC->GetLevel() >= pcAbil[userInput].m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
 		{
-			cout << "Name: " << pcAbil[i].m_name << endl;
-			cout << "Description: " << pcAbil[i].m_desc << endl;
-			cout << "Cool Down: " << pcAbil[i].m_cooldown << endl;
-			cout << "HP Cost: " << pcAbil[i].m_hpCost << endl;
-			cout << "MP Cost: " << pcAbil[i].m_mpCost << endl;
-			cout << "Target: " << pcAbil[i].m_target << endl;
-			cout << "Affects: " << pcAbil[i].m_type << endl;
-			cout << "Damage: " << pcAbil[i].m_amount << endl;
+			cout << "Name: " << pcAbil[userInput].m_name << endl;
+			cout << "Description: " << pcAbil[userInput].m_desc;
+			cout << "Cool Down: " << pcAbil[userInput].m_cooldown << endl;
+			cout << "HP Cost: " << pcAbil[userInput].m_hpCost << endl;
+			cout << "MP Cost: " << pcAbil[userInput].m_mpCost << endl;
+			cout << "Target: " << pcAbil[userInput].m_target << endl;
+			cout << "Affects: " << pcAbil[userInput].m_type << endl;
+			cout << "Amount: " << pcAbil[userInput].m_amount << endl;
 		}
 		else
 		{
 			cout << "That is not an unlocked ability." << endl;
 		}
-	}
-
-	while ( bContinue )
-	{
-		cout << "\nA list of Abilities by name\n" << endl;
-		cout << "Mana Bullet" << endl;
-		cout << "Mana Beam" << endl;
-		cout << "Mana Surge" << endl;
-		cout << "Mana Sync" << endl;
-		cout << "Exit\n" << endl;
-
-
-		for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
-		{
-			if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
-			{
-				cout << "Name: " << it->m_name << endl;
-				cout << "Description: " << it->m_desc << endl;
-				cout << "Cool Down: " << it->m_cooldown << endl;
-				cout << "HP Cost: " << it->m_hpCost << endl;
-				cout << "MP Cost: " << it->m_mpCost << endl;
-				cout << "Target: " << it->m_target << endl;
-				cout << "Affects: " << it->m_type << endl;
-				cout << "Damage: " << it->m_amount << endl;
-				cout << endl;
-			}
-		}
-
-		for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
-		{
-			if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
-			{
-				cout << "Name: " << it->m_name << endl;
-				cout << "Description: " << it->m_desc << endl;
-				cout << "Cool Down: " << it->m_cooldown << endl;
-				cout << "HP Cost: " << it->m_hpCost << endl;
-				cout << "MP Cost: " << it->m_mpCost << endl;
-				cout << "Target: " << it->m_target << endl;
-				cout << "Affects: " << it->m_type << endl;
-				cout << "Damage: " << it->m_amount << endl;
-				cout << endl;
-			}
-		}
-
-
-		for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
-		{
-			if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
-			{
-				cout << "Name: " << it->m_name << endl;
-				cout << "Description: " << it->m_desc << endl;
-				cout << "Cool Down: " << it->m_cooldown << endl;
-				cout << "HP Cost: " << it->m_hpCost << endl;
-				cout << "MP Cost: " << it->m_mpCost << endl;
-				cout << "Target: " << it->m_target << endl;
-				cout << "Affects: " << it->m_type << endl;
-				cout << "Damage: " << it->m_amount << endl;
-				cout << endl;
-			}
-		}
-
-		for (vector<ability>::iterator it = pcAbil.begin();	it != pcAbil.end();	++it)
-		{
-			if(PC->GetLevel() >= it->m_unlockLevel)// if PC->GetLevel() is greater than or equal to it->m_unlockedLevel
-			{
-				cout << "Name: " << it->m_name << endl;
-				cout << "Description: " << it->m_desc << endl;
-				cout << "Cool Down: " << it->m_cooldown << endl;
-				cout << "HP Cost: " << it->m_hpCost << endl;
-				cout << "MP Cost: " << it->m_mpCost << endl;
-				cout << "Target: " << it->m_target << endl;
-				cout << "Affects: " << it->m_type << endl;
-				cout << "Damage: " << it->m_amount << endl;
-				cout << endl;
-			}
-		}
-	}
-
-	if( userInput == 5 )
-	{
-		Interface::GetInstance()->MainGame();
+		cout << endl;
 	}
 }
 
