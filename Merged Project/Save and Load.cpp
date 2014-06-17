aacb7c3884da24f332663b27fbcd9ebdc9e6510a
BigGuy00 <darkcarravenger@yahoo.com>
2014-06-17 19:57:14 -0400

    Revert "an update to save and load"
    This reverts commit 7b2d49d4773660539bbda87523475e0d89ed83bb.

--- Merged Project/Save and Load.cpp
+++ Merged Project/Save and Load.cpp
@@ -56,44 +56,38 @@ void Save()
 
 
 		//ifstream myfile ("NPC.txt");
-		ofstream savefile ("NPCload.txt");
+		ofstream loadfile ("NPCload.txt");
 		string line;
 
-		if (savefile.is_open())
+		if (loadfile.is_open())
 		{	
 			for(int a = 0; a < World::GetInstance()->RoomCount(); a++)
 			{
 				if(World::GetInstance()->GetRooms()[a]->NpcList().size() > 0)
 				{
-					savefile << World::GetInstance()->GetRooms()[a]->GetName() << "\n";
+					loadfile << World::GetInstance()->GetRooms()[a]->GetName() << "\n";
 
 					for(int b = 0; b < World::GetInstance()->GetRooms()[a]->NpcList().size(); b++)
 					{
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetName() << "\n";					
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetDesc() << "\n";
-
-						if(World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetLife() == "")
-						{
-							World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->SetLife("A");
-						}
-
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetLife() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxHp() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxMp() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxStr() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxAgi() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxAs() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetHp() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMp() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetStr() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetAgi() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetAs() << "\n";
-						savefile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetExp() << "\n" << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetName() << "\n";					
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetDesc() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetLife() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxHp() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxMp() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxStr() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxAgi() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMaxAs() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetHp() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetMp() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetStr() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetAgi() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetAs() << "\n";
+						loadfile << World::GetInstance()->GetRooms()[a]->NpcList()[b].GetStats()->GetExp() << "\n" << "\n";
 					}
 				}
 			}
 			
-			savefile.close();
+			loadfile.close();
 		}		
 
 		cout << endl << "Save Completed" << endl << endl;
@@ -148,14 +142,19 @@ void Load()
 			temp.SetExp(atoi(line.c_str()));
 			getline (myfile,line);
 			temp.SetHp(temp.GetHp()-atoi(line.c_str())); //reduce the player's stats by the equipment boost so that it isn't doubled when items are loaded
+			temp.SetMaxHp(temp.GetMaxHp()-atoi(line.c_str()));
 			getline (myfile,line);
 			temp.SetMp(temp.GetMp()-atoi(line.c_str()));
+			temp.SetMaxMp(temp.GetMaxMp()-atoi(line.c_str()));
 			getline (myfile,line);
 			temp.SetAgi(temp.GetAgi()-atoi(line.c_str()));
+			temp.SetMaxAgi(temp.GetMaxAgi()-atoi(line.c_str()));
 			getline (myfile,line);
 			temp.SetStr(temp.GetStr()-atoi(line.c_str()));
+			temp.SetMaxStr(temp.GetMaxStr()-atoi(line.c_str()));
 			getline (myfile,line);
 			temp.SetAs(temp.GetAs()-atoi(line.c_str()));
+			temp.SetMaxAs(temp.GetMaxAs()-atoi(line.c_str()));
 
 			PC->SetStats(temp);
 
